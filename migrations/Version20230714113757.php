235600ac28dcba6061d4994d16dd8be3acd08941
Grzesiek <kiebasag@gmail.com>
2023-07-14 16:01:50 +0200

    slug

--- migrations/Version20230714113757.php
+++ migrations/Version20230714113757.php
@@ -21,15 +21,9 @@ final class Version20230714113757 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('
-        CREATE TABLE sessions (
-            sess_id VARCHAR(128) NOT NULL PRIMARY KEY,
-            sess_data BYTEA NOT NULL,
-            sess_lifetime INTEGER NOT NULL,
-            sess_time INTEGER NOT NULL
-        )
-    ');
-        $this->addSql('CREATE INDEX expiry ON sessions (sess_lifetime)');
+        $this->addSql('ALTER TABLE conference ADD slug VARCHAR(255)');
+        $this->addSql("UPDATE conference SET slug=CONCAT(LOWER(city), '-', year)");
+        $this->addsql('ALTER TABLE conference ALTER COLUMN slug SET NOT NULL');
     }
 
     public function down(Schema $schema): void
